c2bc9c6bb975472e372475456d2cfd54182bef65
Your Name <you@example.com>
2022-05-05 18:51:38 -0400

    added projects and changed about page

--- public/javascripts/style.js
+++ public/javascripts/style.js
@@ -9,3 +9,21 @@
 AOS.init({
   
 });
+
+(function (window, document) {
+  const markers = document.querySelectorAll('mark');
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach((entry) => {
+      if (entry.intersectionRatio > 0) {
+        entry.target.style.animationPlayState = 'running';
+        observer.unobserve(entry.target);
+      }
+    });
+  }, {
+    threshold: 0.8
+  });
+  
+  markers.forEach(mark => {
+    observer.observe(mark);
+  });
+})(window, document);
--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,25 @@ router.get('/', function(req, res, next) {
   });
 });
 
+// shibu page
+router.get('/shibu', function(req, res, next) {
+  res.render('shibu', { 
+    title: 'Express' 
+  });
+});
+
+// shibu page
+router.get('/junk', function(req, res, next) {
+  res.render('junk', { 
+    title: 'Express' 
+  });
+});
+
+// about page
+router.get('/about', function(req, res, next) {
+  res.render('about', { 
+    title: 'Express' 
+  });
+});
+
 module.exports = router;
