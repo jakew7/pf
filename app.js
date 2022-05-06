506570352ba4761c1f09760e656a371e33792f55
Your Name <you@example.com>
2022-05-06 01:05:58 -0400

    update

--- app.js
+++ app.js
@@ -41,4 +41,13 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+if(process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+  })
+}
+
 module.exports = app;
